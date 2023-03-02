bc33a4383a3e3e0ab10f8d935ee45cf0ed26155f
yogitha-vennamaneni <122859439+yogitha-vennamaneni@users.noreply.github.com>
2023-03-02 22:16:34 +1100

    Testing Git Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
     return (
         <div>
-            <h1>My React Application.</h1>
+            <h1>My New React Application.</h1>
         </div>
     );
 }
